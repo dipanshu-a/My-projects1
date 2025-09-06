3b5cf90b82cf79f1d07d22eab8f9fcdde2795ee9
Dipanshu Kumar <dipanshu800a@gmail.com>
2025-09-06 19:22:01 +0530

    Add files via upload

---
+++ pizza dilevery.py
@@ -0,0 +1,22 @@
+
+# WRITE A PROGRAM FOR PIZZA DILIVERY
+
+print(" Welcome to the pizza delivery ")
+
+size = input(" enter your pizza size S M L")
+
+if( size == "S"):
+    
+    print(" you pay 50 rupees WELCOME")
+
+elif( size == "M"):
+
+    print("you pay 100r WELCOME")
+
+elif( size == "L"):
+
+    print("you pay 500r")
+
+else:
+
+    print( "wrong choice")
